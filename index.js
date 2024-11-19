c59f281634c54286450f58d3e9881de425598732
itsshailvi <itsshailvi@gmail.com>
2024-11-19 22:42:57 +0530

    first

---
+++ index.js
@@ -0,0 +1,18 @@
+// index.js (backend)
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = 3400;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/test', (req, res) => {
+    res.json({ message: 'Hello, World! mini' });
+  });
+
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
